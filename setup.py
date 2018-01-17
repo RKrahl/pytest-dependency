36120eba73287293314b4bc55b7c96fdbcbc2a76
Rolf Krahl <rolf@rotkraut.de>
2018-01-17 20:15:12 +0100

    Fix: the unpatched sources do not support Python 2.6.

--- setup.py
+++ setup.py
@@ -69,7 +69,6 @@ setup(
         'Topic :: Software Development :: Testing',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
