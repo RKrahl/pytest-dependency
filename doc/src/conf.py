e56c4e384a285451004d012aed35f3d8e4216f1f
Rolf Krahl <rolf@rotkraut.de>
2020-02-14 21:55:24 +0100

    Update copyright date.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pytest-dependency'
-copyright = u'2016-2018, Rolf Krahl'
+copyright = u'2016-2020, Rolf Krahl'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
