286cbf02c36b7d5117514f64d0067f8a870d3b59
Rolf Krahl <rolf@rotkraut.de>
2020-04-11 15:12:40 +0200

    Do the substitutions of the doc string and version into pytest_dependency.py also also for setup.py build.

--- setup.py
+++ setup.py
@@ -6,13 +6,16 @@ some tests as dependent from other tests.  These tests will then be
 skipped if any of the dependencies did fail or has been skipped.
 """
 
+from distutils.cmd import Command as du_cmd
 import distutils.log
 import os
 import os.path
 import re
+import stat
 import string
 from setuptools import setup
-import setuptools.command.sdist as st_sdist
+import setuptools.command.build_py
+import setuptools.command.sdist
 try:
     import setuptools_scm
     version = setuptools_scm.get_version()
@@ -26,19 +29,47 @@ except (ImportError, LookupError):
         distutils.log.warn("warning: cannot determine version number")
         version = "UNKNOWN"
 
+doc_string = __doc__
 
-class sdist(st_sdist.sdist):
-    def make_release_tree(self, base_dir, files):
-        st_sdist.sdist.make_release_tree(self, base_dir, files)
-        if not self.dry_run:
-            src = "pytest_dependency.py"
-            dest = os.path.join(base_dir, src)
-            if hasattr(os, 'link') and os.path.exists(dest):
-                os.unlink(dest)
-            subst = {'DOC': __doc__, 'VERSION': version}
-            with open(src, "rt") as sf, open(dest, "wt") as df:
-                df.write(string.Template(sf.read()).substitute(subst))
+class copy_file_mixin:
+    """Distutils copy_file() mixin.
 
+    Inject a custom version version of the copy_file() method that
+    does some substitutions on the fly into distutils command class
+    hierarchy.
+    """
+    Subst_srcs = {"pytest_dependency.py"}
+    Subst = {'DOC': doc_string, 'VERSION': version}
+    def copy_file(self, infile, outfile, preserve_mode=1, preserve_times=1,
+                  link=None, level=1):
+        if infile in self.Subst_srcs:
+            fstat = os.stat(infile)
+            if os.path.basename(outfile) == os.path.basename(infile):
+                distutils.log.info("copying (with substitutions) %s -> %s",
+                                   infile, os.path.dirname(outfile))
+            else:
+                distutils.log.info("copying (with substitutions) %s -> %s",
+                                   infile, outfile)
+            if not self.dry_run:
+                if os.path.exists(outfile):
+                    os.unlink(outfile)
+                with open(infile, "rt") as sf, open(outfile, "wt") as df:
+                    df.write(string.Template(sf.read()).substitute(self.Subst))
+                if preserve_mode:
+                    os.chmod(outfile, stat.S_IMODE(fstat[stat.ST_MODE]))
+            return (outfile, 1)
+        else:
+            # Note: can't use super() with Python 2.
+            return du_cmd.copy_file(self, infile, outfile,
+                                    preserve_mode=preserve_mode,
+                                    preserve_times=preserve_times,
+                                    link=link, level=level)
+
+class build_py(copy_file_mixin, setuptools.command.build_py.build_py):
+    pass
+
+class sdist(copy_file_mixin, setuptools.command.sdist.sdist):
+    pass
 
 setup(
     name='pytest-dependency',
@@ -50,7 +81,7 @@ setup(
     maintainer_email='rolf@rotkraut.de',
     url='https://github.com/RKrahl/pytest-dependency',
     license='Apache Software License 2.0',
-    long_description=__doc__,
+    long_description=doc_string,
     project_urls={
         'Documentation': 'https://pytest-dependency.readthedocs.io/',
         'Source Code': 'https://github.com/RKrahl/pytest-dependency',
@@ -79,5 +110,5 @@ setup(
             'dependency = pytest_dependency',
         ],
     },
-    cmdclass = {'sdist': sdist},
+    cmdclass = {'build_py': build_py, 'sdist': sdist},
 )
