6835cf4369630ea8d98aa3c4bd3addb6c0e2713e
Rolf Krahl <rolf@rotkraut.de>
2021-12-29 13:43:36 +0100

    Remove dependency on the pytest version in tests of documentation examples

--- tests/test_09_examples_names.py
+++ tests/test_09_examples_names.py
@@ -2,9 +2,7 @@
 """
 
 import pytest
-from conftest import get_example, require_pytest_version
-
-require_pytest_version("4.2.0")
+from conftest import get_example
 
 
 def test_nodeid(ctestdir):
@@ -13,12 +11,15 @@ def test_nodeid(ctestdir):
     with get_example("nodeid.py").open("rt") as f:
         ctestdir.makepyfile(f.read())
     result = ctestdir.runpytest("--verbose")
-    result.assert_outcomes(passed=6, skipped=0, failed=0, xfailed=1)
+    try:
+        result.assert_outcomes(passed=6, skipped=0, failed=0, xfailed=1)
+    except TypeError:
+        result.assert_outcomes(passed=6, skipped=0, failed=0)
     result.stdout.re_match_lines(r"""
         .*::test_a PASSED
         .*::test_b\[7-True\] PASSED
         .*::test_b\[0-False\] PASSED
-        .*::test_b\[-1-False\] XFAIL(?:\s+\(.*\))?
+        .*::test_b\[-1-False\] (?:XFAIL(?:\s+\(.*\))?|xfail)
         .*::TestClass::test_c PASSED
         .*::TestClass::test_d\[order\] PASSED
         .*::TestClass::test_d\[disorder\] PASSED
