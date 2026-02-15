010c05fd1ab4df00c620ff267cda5382f356f90f
Rolf Krahl <rolf@rotkraut.de>
2026-02-15 18:09:54 +0100

    Update Pyhthon versions in trove classifiers

--- setup.py
+++ setup.py
@@ -158,6 +158,8 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Topic :: Software Development :: Testing",
     ],
     project_urls = dict(
